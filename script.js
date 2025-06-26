cadcc941637cae32678c5c0fef769a07ca949dae
Dabeey <abbasdaughter18@gmail.com>
2025-06-26 23:55:35 +0100

    Implement drag and drop functionality and fix CSS syntax error

---
+++ script.js
@@ -0,0 +1,18 @@
+let list = document.getElementById('list');
+let rightBox = document.getElementById('right');
+let leftBox = document.getElementById('left');
+
+for (list of lists){
+    list.addEventListener('dragstart' function(e) {
+        let selected = e.target;
+
+        // Add the 'dragging' class to the selected item
+        rightBox.addEventListener('dragover', function(e) {
+            e.preventDefault();
+
+        rightBox.addEventListener('drop', function(e) {
+            rightBox.appendChild(selected);
+        });
+    })
+};
+
